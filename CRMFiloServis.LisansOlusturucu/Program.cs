81f3d1760b3004a057bb4cf820f8acdb86f29d7c
muratk <muratk@DESKTOP-GJUJ5JR>
2026-03-28 05:04:47 +0300

    Dashboard veri yükleme ve Türkçe karakter düzeltmeleri yapıldı

---
+++ CRMFiloServis.LisansOlusturucu/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace CRMFiloServis.LisansOlusturucu;
+
+internal static class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("CRMFiloServis Lisans Olusturucu");
+    }
+}
